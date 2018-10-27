76cae675066d748abbfed7e5abd91754fba01716
Oleksandr Marushchak <35522827+noxend@users.noreply.github.com>
2018-10-27 17:29:02 +0300

    removing and editing items

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 console.log(todos.length || "none");
 
-const todosList = document.getElementById("todos-list");
-const counItems = document.getElementById("count-items");
-
 function createElement(tag, probs, ...children) {
   const element = document.createElement(tag);
   Object.keys(probs).forEach(key => {
@@ -39,22 +36,73 @@ function createTodoItem(confirm, title, id) {
     createElement("span", {}),
   );
 
+
+  const editInput = createElement("input", { className: "text-edit", type: "text" });
   const span = createElement("span", { className: "task" }, title);
 
   const groupBtn = createElement("div", { className: "btn-group" },
-    createElement("button", { className: "menu" },
-      createElement('i', {className: "fas fa-arrow-left"})
-    ),
+    createElement('i', {className: "fas fa-arrow-left menu"}),
     createElement("button", { className: "edit blue-button" }, 'Edit'),
-    createElement("button", { className: "edit red-button" }, 'Remove'),
+    createElement("button", { className: "remove red-button" }, 'Remove'),
   );
 
-  const li = createElement("li", { className: `todos-item ${confirm ? 'complate' : ''}` }, label, span, groupBtn);
+  const li = createElement("li", { className: `todos-item ${confirm ? 'complate' : ''}` }, label, span, editInput, groupBtn);
 
+  bindEvents(li);
   return li;
 }
 
 document.getElementById('form-add').addEventListener('submit', addTodo);
+
+
+function menuFunc() {
+  const menu = this.parentNode;
+  this.classList.toggle('menu-opened');
+  menu.classList.toggle('opened');
+}
+
+
+function editTodoItem() {
+  const item = this.parentNode.parentNode;
+  const task = item.querySelector('.task');
+  const editInput = item.querySelector('.text-edit');
+  const isEditing = item.classList.contains('editing');
+
+  if (isEditing) {
+    task.innerText = editInput.value;
+    this.innerText = "Edit";
+
+  } else {
+    editInput.value = task.innerText;
+    this.innerText = "Save";
+  }
+
+  item.classList.toggle("editing");
+  
+}
+
+function removeTodoItem() {
+  const item = this.parentNode.parentNode;
+  todosList.removeChild(item);
+  counItems.innerText = getCountTodos();
+}
+
+function toggleTodoItem() {
+  const item = this.parentNode.parentNode;
+  item.classList.toggle('complate');
+}
+
+function bindEvents(todoItem) {
+  const menu = todoItem.querySelector('.menu');
+  const edit = todoItem.querySelector('.edit');
+  const remove = todoItem.querySelector('.remove');
+  const checkbox = todoItem.querySelector('.my-checkbox input');
+  
+  menu.addEventListener('click', menuFunc);
+  edit.addEventListener('click', editTodoItem);
+  remove.addEventListener('click', removeTodoItem);
+  checkbox.addEventListener('click', toggleTodoItem);
+}
 
 function addTodo(e) {
   e.preventDefault();
@@ -63,10 +111,6 @@ function addTodo(e) {
   counItems.innerText = getCountTodos();  
   save({ title: input.value, confirm: false });
   input.value = '';
-}
-
-function removeTodo() {
-
 }
 
 function save(todo) {
@@ -89,6 +133,15 @@ function load() {
   return todosList;
 }
 
+const todosList = document.getElementById("todos-list");
+const counItems = document.getElementById("count-items");
+const todoItems = document.querySelectorAll(".todos-item");
 
-load();
+function main() {
+  todoItems.forEach(item => bindEvents(item));
+  counItems.innerText = getCountTodos();
+}
+
+main();
+// load();
 
